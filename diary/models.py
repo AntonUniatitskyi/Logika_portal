1ef77082b74990c6bf67e4350a295bbfc9f7b97c
Anton <antonh1803@gmail.com>
2025-03-07 20:52:51 +0200

    moved model Subject

--- diary/models.py
+++ diary/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
+from home import models as home_models
 
 # Create your models here.
-class Subject(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Предмет')
+# class Subject(models.Model):
+#     name = models.CharField(max_length=100, verbose_name='Предмет')
 
-    def __str__(self):
-        return f"{self.name}"
+#     def __str__(self):
+#         return f"{self.name}"
 
 class Mark(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='student_marks')
     mark = models.PositiveIntegerField(verbose_name='Оцінка', validators=[MinValueValidator(1), MaxValueValidator(12)])
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='student_subject')
+    subject = models.ForeignKey(home_models.Subject, on_delete=models.CASCADE, related_name='student_subject')
 
     def __str__(self):
         return f"{self.student}, {self.mark}, {self.subject}"
--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Предмет')
+
+    def __str__(self):
+        return f"{self.name}"
