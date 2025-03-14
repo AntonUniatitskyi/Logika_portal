7832946f1464fcd0718fbfe4ea10e490f8471247
Artem Zamotaev <artemmori2905@gmail.com>
2025-03-14 10:50:41 +0200

    new visual functionality

--- events_calendar/forms.py
+++ events_calendar/forms.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 from django.forms import Select, ModelForm, NumberInput, DateInput, TimeInput, TextInput
+=======
+from django.forms import Select, ModelForm, NumberInput, DateInput, TimeInput
+>>>>>>> 4653137 (new visual functionality)
 from . import models
 
 class EventForm(ModelForm):
@@ -8,8 +12,14 @@ class EventForm(ModelForm):
         widgets = {
             'group': Select(attrs={'class': 'form-control'}),
             'subject': Select(attrs={'class': 'form-control'}),
+<<<<<<< HEAD
             'date':DateInput(attrs={'class': 'form-control','placeholder': 'Дата'}),
             'start_time':TimeInput(attrs={'class': 'form-control','placeholder': 'Час початку'}),
             'description':TextInput(attrs={'class': 'form-control','placeholder': 'Домашнє завдання'}),
+=======
+            'date':DateInput(atts={'class': 'form-control','placeholder': 'Дата'}),
+            'start_time':TimeInput(atts={'class': 'form-control','placeholder': 'Час початку'}),
+            'mark': NumberInput(attrs={'class': 'form-control', 'placeholder': 'Оцінка'}),
+>>>>>>> 4653137 (new visual functionality)
         }
         
